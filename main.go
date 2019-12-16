8c962a3653a65d99e0c6a60f4eac5da8c2c831b3
magodo <wztdyl@sina.com>
2019-12-16 13:36:19 +0800

    remove comment

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 )
 
 func main() {
-	// enable tracer
 	// remove date and time stamp from log output as the plugin SDK already adds its own
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 
